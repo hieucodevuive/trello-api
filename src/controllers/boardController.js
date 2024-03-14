68fdba4bf4c2338991e75e5207559877c8112290
Hieu Pham <hieupham.code@gmail.com>
2024-03-15 01:04:54 +0700

    dnd column BE

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -21,7 +21,18 @@ const getDetails = async(req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const update = async(req, res, next) => {
+  try {
+    const boardId = req.params.id
+    const updatedBoard = await boardService.update(boardId, req.body)
+    //Co ket qua thi tra ve client
+    res.status(StatusCodes.OK).json(updatedBoard)
+  } catch (error) { next(error) }
+}
+
+
 export const boardController = {
   createNew,
-  getDetails
+  getDetails,
+  update
 }
