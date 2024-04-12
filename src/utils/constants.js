23ef52c2b97c97b25e7054ce11f7425d3e859e5e
Hieu Pham <hieupham.code@gmail.com>
2024-04-12 16:43:05 +0700

    Update listdomain again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // deploy domain
-  'https://trello-web-topaz-beta.vercel.app/'
+  'https://trello-web-topaz-beta.vercel.app'
 ]
 
 export const BOARD_TYPES = {
